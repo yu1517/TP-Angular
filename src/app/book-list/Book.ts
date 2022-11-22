ba58063213b9bd50e66f18538c58a87ca3183c3f
yuli <giuliana.delatorre@hotmail.com>
2022-11-21 23:02:20 -0300

    Echo hasta la clase de event y binding

---
+++ src/app/book-list/Book.ts
@@ -0,0 +1,9 @@
+export interface Book {
+    title: string;
+    author: string;
+    price: number;
+    stock: number;
+    image: string;
+    clearance: boolean;
+    quantity: number;
+}
--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from './Book'; //importa la interfas book del archivo book
 
 @Component({
   selector: 'app-book-list',
@@ -6,12 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./book-list.component.scss']
 })
 export class BookListComponent {
-  book = {
-    "title": "Los Siete Maridos de Evelyn Hugo",
-    "author": "Taylor Jenkins",
-    "price": 4.900,
-    "stock": 9,
-    "image": "assets/img/losSieteMaridosdeEvelynHugo.jpg"
-
+  books: Book[] = [
+    {
+      title: "Los Siete Maridos de Evelyn Hugo",
+      author: "Taylor Jenkins",
+      price: 4900,
+      stock: 2,
+      image: "assets/img/losSieteMaridosdeEvelynHugo.jpg",
+      clearance: false,
+      quantity: 0,
+    },
+    {
+      title: "Caballo de Fuego - Paris",
+      author: "Florencia Bonelli",
+      price: 4300,
+      stock: 9,
+      image: "assets/img/caballodefuego-paris.jpg",
+      clearance: true,
+      quantity: 0,
+    }, {
+      title: "Asesinato en el Orient Express",
+      author: "Agatha Christie",
+      price: 3900,
+      stock: 0,
+      image: "assets/img/AsesinatoenelOrientExpress.jpg",
+      clearance: false,
+      quantity: 0,
+    },
+  ];
+  upQuantity(book: Book): void { // este metodo devuelve void porque no lo vamos a utilizar
+    if (book.quantity < book.stock)
+      book.quantity++;
+  }
+  downQuantity(book: Book): void {
+    if (book.quantity > 0)
+      book.quantity--;
+  }
+  changeQuantity(event, book: Book): void {
+    console.log(event.key);
   }
 }
